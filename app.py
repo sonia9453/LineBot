bf6b2db0e08a1486554ced9df8a3f7c6fb7aadb7
sonia9453 <p1973.0702@gmail.com>
2025-08-29 12:46:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -22,8 +22,8 @@ from linebot.v3.webhooks import (
 
 app = Flask(__name__)
 
-configuration = Configuration(access_token='YOUR_CHANNEL_ACCESS_TOKEN')
-handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+configuration = Configuration(access_token='3uCp6ADwnZ0RB+SiooROBOOmpWb32rip0H02Osm2nSAOYxdg1ig1ivyLRhWJE1pVRyZeetSlcNZIxrLkLFk37Nv3xwhaRemRhTvoDjVBmd0rGRb4PiDXOvy/6LBeiRkkIAoyK95lb1Wvxj1gc7+EkAdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('ec4b665efa1e09b9d3a7b3bda95ac34e')
 
 
 @app.route("/callback", methods=['POST'])
@@ -57,4 +57,5 @@ def handle_message(event):
         )
 
 if __name__ == "__main__":
+
     app.run()      # 預設5000
